eb193122aab5addacd1b1dbad11356d3793005b8
kushal rathore <singhkushal235@gmail.com>
2026-08-03 23:23:14 +0530

    feat(auth): implement JWT authentication APIs

--- src/main/java/com/main/MerchantMart/utility/contants/AuthConstants.java
+++ src/main/java/com/main/MerchantMart/utility/contants/AuthConstants.java
@@ -3,7 +3,7 @@ package com.main.MerchantMart.utility.contants;
 public class AuthConstants {
     public static final String PROVIDER_LOCAL = "LOCAL";
     public static final String PROVIDER_GOOGLE = "GOOGLE";
-    public static final String SIGNUP_SUCCESS = "GOOGLE";
-    public static final String LOGIN_SUCCESS = "GOOGLE";
+    public static final String SIGNUP_SUCCESS = "User registered successfully.";
+    public static final String LOGIN_SUCCESS = "Login successful.";
 }
 
